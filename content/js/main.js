52d41406ff6646a803be2a9559bda86ce0a45623
islamnashaat03 <islamnashaat03@gmail.com>
2023-10-10 23:52:14 +0300

    finish today

--- content/js/main.js
+++ content/js/main.js
@@ -6,11 +6,9 @@ const hamburgerIcon = document.querySelector('.nav-bar .toggle-btn'),
   navbarMobile = document.querySelector('.navbar-mobile'),
   overlayAll = document.querySelector('.overlay-all'),
   closeBtn = document.querySelector('.close-btn');
-let catWrapper = document.querySelector('.categories .wrapper');
-let apiUrl =
-  'https://www.luai-phone.com/wp-json/wc/v3/products?consumer_key=ck_efa31b6beedcbcb7718f014d878c4d3b471ac976&consumer_secret=cs_f7c266306d2d9d10f0fb0fbcd8a8eb3d201826a8';
-let apiUrlCategories =
-  'https://www.luai-phone.com/wp-json/wc/v3/products/categories?per_page=20&parent=0&consumer_key=ck_efa31b6beedcbcb7718f014d878c4d3b471ac976&consumer_secret=cs_f7c266306d2d9d10f0fb0fbcd8a8eb3d201826a8&fbclid=IwAR3ePmWjakvO4UJkdeEW9s70m0ihXRTOlzH4Xm_uoQROPrg_iq0jklNuWrE';
+let catWrapper = document.querySelector('.categories  .wrapper ');
+let apiUrl = '';
+let apiUrlCategories = '../cat-api.json';
 // PRE LOADING Function
 setTimeout(function () {
   // loadingDiv.style.opacity = '0';
@@ -54,50 +52,50 @@ $('.main-slider').owlCarousel({
     0: {
       items: 1,
       nav: false,
-      dots: true,
+      dots: false,
     },
     600: {
       items: 1,
       nav: false,
-      dots: true,
+      dots: false,
     },
     1000: {
       items: 1,
       nav: false,
-      dots: true,
+      dots: false,
     },
   },
 });
 // CATEGORIES  SLIDER
-$('.category-slider').owlCarousel({
-  loop: true,
-  margin: 20,
-  autoplay: true,
-  animateIn: 'fadeIn',
-  animateOut: 'fadeOut',
-  navText: [
-    "<i class='owl-btn  fa-solid fa-angle-left '></i>",
-    "<i class='owl-btn  fa-solid fa-angle-right'></i>",
-  ],
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 2,
-      nav: false,
-      dots: true,
-    },
-    600: {
-      items: 4,
-      nav: false,
-      dots: true,
-    },
-    1000: {
-      items: 6,
-      nav: true,
-      dots: true,
-    },
-  },
-});
+// $('.category-slider').owlCarousel({
+//   loop: true,
+//   margin: 20,
+//   autoplay: true,
+//   animateIn: 'fadeIn',
+//   animateOut: 'fadeOut',
+//   navText: [
+//     "<i class='owl-btn  fa-solid fa-angle-left '></i>",
+//     "<i class='owl-btn  fa-solid fa-angle-right'></i>",
+//   ],
+//   responsiveClass: true,
+//   responsive: {
+//     0: {
+//       items: 2,
+//       nav: false,
+//       dots: true,
+//     },
+//     600: {
+//       items: 4,
+//       nav: false,
+//       dots: true,
+//     },
+//     1000: {
+//       items: 6,
+//       nav: true,
+//       dots: true,
+//     },
+//   },
+// });
 // LATEST PRODUCTS SLIDER
 $('.latest-products-slider').owlCarousel({
   loop: true,
@@ -188,8 +186,9 @@ $('.brands-slider').owlCarousel({
     },
   },
 });
-// START FETCHING DATA
+$('.category-slider').on('initialized.owl.carousel', function () {});
 
+// START FETCHING DATA
 async function fetchData(e) {
   try {
     let result = await fetch(e);
@@ -200,20 +199,19 @@ async function fetchData(e) {
   }
 }
 
-// function showData(datas) {
-//   datas.forEach((data) => {
-//     console.log(data);
-//     catWrapper.innerHTML += `<li>
-//     <a href="">
-//       <figure class="image-wrap">
-//         <img src="${data.image}" alt="Category Image">
-//       </figure>
-//       <h4>${data.name}</h4>
-//     </a>
-//   </li>`;
-//   });
-// }
-
-// fetchData(apiUrlCategories);
+function showData(datas) {
+  datas.forEach((data) => {
+    console.log(data);
+    // catWrapper.innerHTML += `<li>
+    //   <a href="${data.href}">
+    //     <figure class="image-wrap">
+    //       <img src="${data.image.src}" alt="Category Image">
+    //     </figure>
+    //     <h4>${data.name}</h4>
+    //   </a>
+    // </li>`;
+  });
+}
+fetchData(apiUrlCategories);
 
 // SHOW CATEGORIES
